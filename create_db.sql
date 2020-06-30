511177f9e6ac64824d6eb4580430f48758f4613b
M042 <boukobza.nathan@gmail.com>
2020-06-30 14:28:19 +0200

    Project ready

--- create_db.sql
+++ create_db.sql
@@ -1,6 +1,8 @@
 
+CREATE SEQUENCE public.aliment_id_seq;
+
 CREATE TABLE public.Aliment (
-                id INTEGER NOT NULL,
+                id INTEGER NOT NULL DEFAULT nextval('public.aliment_id_seq'),
                 Name VARCHAR(100) NOT NULL,
                 Price NUMERIC(9,2) NOT NULL,
                 Category VARCHAR(45) NOT NULL,
@@ -10,12 +12,16 @@ CREATE TABLE public.Aliment (
 );
 
 
+ALTER SEQUENCE public.aliment_id_seq OWNED BY public.Aliment.id;
+
 CREATE INDEX aliment_idx
  ON public.Aliment
  ( Category );
 
+CREATE SEQUENCE public.ingredient_id_seq;
+
 CREATE TABLE public.Ingredient (
-                id INTEGER NOT NULL,
+                id INTEGER NOT NULL DEFAULT nextval('public.ingredient_id_seq'),
                 Name VARCHAR(100) NOT NULL,
                 Unit VARCHAR(45) NOT NULL,
                 Price_per_unit NUMERIC(9,2) NOT NULL,
@@ -23,6 +29,8 @@ CREATE TABLE public.Ingredient (
                 CONSTRAINT ingredient_pk PRIMARY KEY (id)
 );
 
+
+ALTER SEQUENCE public.ingredient_id_seq OWNED BY public.Ingredient.id;
 
 CREATE TABLE public.Recipe (
                 Aliment_id INTEGER NOT NULL,
@@ -39,7 +47,7 @@ CREATE TABLE public.Adress (
                 id INTEGER NOT NULL DEFAULT nextval('public.adress_id_seq'),
                 Name VARCHAR(100) NOT NULL,
                 Adress VARCHAR(200) NOT NULL,
-                ZipCode INTEGER NOT NULL,
+                ZipCode VARCHAR(10) NOT NULL,
                 City VARCHAR(100) NOT NULL,
                 CONSTRAINT adress_pk PRIMARY KEY (id)
 );
@@ -47,8 +55,10 @@ CREATE TABLE public.Adress (
 
 ALTER SEQUENCE public.adress_id_seq OWNED BY public.Adress.id;
 
+CREATE SEQUENCE public.restaurant_id_seq;
+
 CREATE TABLE public.Restaurant (
-                id INTEGER NOT NULL,
+                id INTEGER NOT NULL DEFAULT nextval('public.restaurant_id_seq'),
                 Name VARCHAR(100) NOT NULL,
                 Description VARCHAR(1000),
                 Phone VARCHAR(20),
@@ -57,6 +67,8 @@ CREATE TABLE public.Restaurant (
                 CONSTRAINT restaurant_pk PRIMARY KEY (id)
 );
 
+
+ALTER SEQUENCE public.restaurant_id_seq OWNED BY public.Restaurant.id;
 
 CREATE TABLE public.Stock (
                 Restaurant_id INTEGER NOT NULL,
@@ -71,7 +83,8 @@ CREATE SEQUENCE public.person_id_seq;
 CREATE TABLE public.Person (
                 id INTEGER NOT NULL DEFAULT nextval('public.person_id_seq'),
                 Name VARCHAR(100) NOT NULL,
-                Password VARCHAR(45) NOT NULL,
+                Last_name VARCHAR(100) NOT NULL,
+                Password VARCHAR(255) NOT NULL,
                 Email VARCHAR(200) NOT NULL,
                 CONSTRAINT person_pk PRIMARY KEY (id)
 );
@@ -79,34 +92,48 @@ CREATE TABLE public.Person (
 
 ALTER SEQUENCE public.person_id_seq OWNED BY public.Person.id;
 
+CREATE SEQUENCE public.user_id_seq;
+
 CREATE TABLE public.Employee (
-                Person_id INTEGER NOT NULL,
+                Person_id INTEGER NOT NULL DEFAULT nextval('public.user_id_seq'),
                 Role VARCHAR(45) NOT NULL,
                 Restaurant_id INTEGER NOT NULL,
                 CONSTRAINT employee_pk PRIMARY KEY (Person_id)
 );
 
 
+ALTER SEQUENCE public.user_id_seq OWNED BY public.Employee.Person_id;
+
+CREATE SEQUENCE public.user_id_seq;
+
 CREATE TABLE public.Delivery_Men (
-                Employe_id INTEGER NOT NULL,
+                Employe_id INTEGER NOT NULL DEFAULT nextval('public.user_id_seq'),
                 Vehicle VARCHAR(45) NOT NULL,
                 CONSTRAINT delivery_men_pk PRIMARY KEY (Employe_id)
 );
 
 
+ALTER SEQUENCE public.user_id_seq OWNED BY public.Delivery_Men.Employe_id;
+
+CREATE SEQUENCE public.user_id_seq;
+
 CREATE TABLE public.Customer (
-                Person_id INTEGER NOT NULL,
+                Person_id INTEGER NOT NULL DEFAULT nextval('public.user_id_seq'),
                 Phone VARCHAR(20),
                 Adress_id INTEGER NOT NULL,
                 CONSTRAINT customer_pk PRIMARY KEY (Person_id)
 );
 
 
+ALTER SEQUENCE public.user_id_seq OWNED BY public.Customer.Person_id;
+
+CREATE SEQUENCE public.purchase_id_seq;
+
 CREATE TABLE public.Purchase (
-                id INTEGER NOT NULL,
+                id INTEGER NOT NULL DEFAULT nextval('public.purchase_id_seq'),
                 State VARCHAR(45) NOT NULL,
                 Date TIMESTAMP NOT NULL,
-                Paid_Price NUMERIC(9,2) NOT NULL,
+                Paid_Price NUMERIC(9,2),
                 Restaurant_id INTEGER NOT NULL,
                 Person_id INTEGER NOT NULL,
                 Adress_id INTEGER NOT NULL,
@@ -114,17 +141,23 @@ CREATE TABLE public.Purchase (
 );
 
 
+ALTER SEQUENCE public.purchase_id_seq OWNED BY public.Purchase.id;
+
 CREATE INDEX order_idx
  ON public.Purchase
  ( State, Restaurant_id, Person_id );
 
+CREATE SEQUENCE public.purchase_aliment_id_seq;
+
 CREATE TABLE public.Purchase_Aliment (
-                id INTEGER NOT NULL,
+                id INTEGER NOT NULL DEFAULT nextval('public.purchase_aliment_id_seq'),
                 Purchase_id INTEGER NOT NULL,
                 Aliment_id INTEGER NOT NULL,
                 CONSTRAINT purchase_aliment_pk PRIMARY KEY (id)
 );
 
+
+ALTER SEQUENCE public.purchase_aliment_id_seq OWNED BY public.Purchase_Aliment.id;
 
 ALTER TABLE public.Recipe ADD CONSTRAINT aliment_recipe_fk
 FOREIGN KEY (Aliment_id)
